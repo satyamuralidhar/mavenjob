02e492843d4016f20a93b85bb4a8aef97f097630
k8sbot <muralidhar.peddireddi@gmail.com>
2023-07-17 08:45:11 +0530

    docker

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,4 @@
+def call(string username,string imagetag,string imagename){
+    docker build -t ${imagename}:${imagetag}
+    docker tag ${imagename}:${imagetag} ${username}/${imagename}:${imagetag}
+}
